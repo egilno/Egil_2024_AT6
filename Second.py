4f56fe877bf345c3c29364c59548e860d975eef9
Egil Osmundsen <notoegil@gmail.com>
2024-12-01 02:23:42 +0100

    Final commit with the called validation of the integers

--- Second.py
+++ Second.py
@@ -2,3 +2,12 @@ from Task1 import UserInputValidator
 
 validation1 = UserInputValidator()
 validation2 = UserInputValidator()
+
+is_it_integers1 = [13, "Love", "Hate", 2, -2]
+is_it_integers2 = ["Exam", 20, 999, "Hope", 0]
+
+valid1 = validation1.validate_positive_integers(is_it_integers1)
+valid2 = validation2.validate_positive_integers(is_it_integers2)
+
+print(valid1)
+print(valid2)
